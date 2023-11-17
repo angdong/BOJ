8c95d9fc035360a026fd17149379df9fd60584b7
Euidong Yang <100507452+angdong@users.noreply.github.com>
2023-11-17 18:14:59 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4556 ms, Memory: 9832 KB -BaekjoonHub

--- 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -1,19 +1,22 @@
 #include<iostream>
-#include<algorithm>
-
-int N;
 
 using namespace std;
 
-int arr[1000001];
+int arr[2000001];
+
 int main(){
-    ios::sync_with_stdio(0);
+    ios_base::sync_with_stdio(0);
     cin.tie(0);
+    int N;
     cin >> N;
-    for(int i=0; i<N; i++){
-        cin >> arr[i];
+    while(N--){
+        int tmp;
+        cin >> tmp;
+        arr[tmp+1000000]++;
     }
-    sort(arr, arr+N);
-    for(int i=0; i<N; i++)
-        cout << arr[i] << "\n";
+    for(int i=0; i<2000001; i++){
+        while(arr[i]--){
+            cout << i-1000000 << "\n";
+        }
+    }
 }
